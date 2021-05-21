4a30494b8040a7c45a300407abe10618d0e9a913
Aziz <Aziz>
2021-05-21 14:28:37 +0200

    new f on the new b

--- QuickStart.java
+++ QuickStart.java
@@ -1,5 +1,7 @@
 class QuickStart {
     public static void main(String[] args) {
-        System.out.println("Hello, World.");
+        System.out.println("sexy, World.");
+        System.out.println("u will get a d from me, in this World.");
+
     }
 }
